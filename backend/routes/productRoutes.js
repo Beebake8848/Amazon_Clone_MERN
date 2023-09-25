88b35af91c9ad3efea8e86432ce7428469c6b005
Beebake8848 <kbibek448@gmail.com>
2023-09-25 22:53:10 +0545

    added

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -8,4 +8,21 @@ productRouter.get("/", async (req, res) => {
   res.send(products);
 });
 
+productRouter.get("/slug/:slug", async (req, res) => {
+  const product = await Product.findOne({ slug: req.params.slug });
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product Not Found" });
+  }
+});
+productRouter.get("/:id", async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product Not Found" });
+  }
+});
+
 export default productRouter;
